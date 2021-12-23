a003f06f998a6fe72c0372235cbde698e50aa237
unknown <selbiger10@gmail.com>
2021-12-23 15:12:42 +0200

    Fixed servers/VPC typo

--- Terraform/workspaces/VPC/main.tf
+++ Terraform/workspaces/VPC/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source             = "app.terraform.io/opsschool-ben/vpc/aws"
-  version            = var.servers_module_version
+  version            = var.vpc_module_version
   vpc_name           = var.vpc_name
   cidr_size          = var.cidr_size
   availability_zones = var.availability_zones
