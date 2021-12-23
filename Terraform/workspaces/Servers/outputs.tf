e175d1e0798b08c4cb17e4befc366711e04e73ee
unknown <selbiger10@gmail.com>
2021-12-23 16:20:12 +0200

    Fixed output

--- Terraform/workspaces/Servers/outputs.tf
+++ Terraform/workspaces/Servers/outputs.tf
@@ -1,39 +1,39 @@
 output "consul_servers_private_ips" {
   description = "Private IP's of the Consul servers"
-  value       = aws_instance.consul_servers.*.private_ip
+  value       = module.servers.consul_servers_private_ips
 }
 
 output "jenkins_server_private_ip" {
   description = "Private IP of the Jenkins server"
-  value       = aws_instance.jenkins_server.*.public_ip
+  value       = module.servers.jenkins_server_private_ip
 }
 
 output "jenkins_nodes_private_ip" {
   description = "Private IP's of the Jenkins nodes"
-  value       = aws_instance.jenkins_nodes.*.private_ip
+  value       = module.servers.jenkins_nodes_private_ip
 }
 
 output "bastion_server_public_ip" {
   description = "Public IP of the Bastion host"
-  value       = aws_isntace.bastion_server.*.public_ip
+  value       = module.servers.bastion_server_public_ip
 }
 
 output "bastion_server_private_ip" {
   description = "Private IP of the Bastion host"
-  value       = aws_isntace.bastion_server.*.private_ip
+  value       = module.servers.bastion_server_private_ip
 }
 
 output "ansible_server_private_ip" {
   description = "Private IP of the Ansible server"
-  value       = aws_isntace.ansible_server.*.private_ip
+  value       = module.servers.ansible_server_private_ip
 }
 
 output "consul_alb_public_dns" {
   description = "Consul ALB Public DNS name"
-  value       = aws_alb.consul_alb.dns_name
+  value       = module.servers.consul_alb_public_dns
 }
 
 output "jenkins_alb_public_dns" {
   description = "Jenkins ALB Public DNS name"
-  value       = aws_alb.jenkins_alb.dns_name
+  value       = module.servers.jenkins_alb_public_dns
 }
