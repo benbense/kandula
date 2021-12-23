1e183d684198d6a5a19440ae2c5af4123440f620
unknown <selbiger10@gmail.com>
2021-12-23 16:05:32 +0200

    Added IAM instance profile name reference

--- Terraform/workspaces/Servers/main.tf
+++ Terraform/workspaces/Servers/main.tf
@@ -8,14 +8,15 @@ data "terraform_remote_state" "vpc" {
   }
 }
 module "servers" {
-  source               = "app.terraform.io/kandula/servers/aws"
-  version              = "1.0.1"
-  vpc_id               = data.terraform_remote_state.vpc.outputs.vpc_id
-  available_zone_names = data.terraform_remote_state.vpc.outputs.available_zone_names
-  public_subnets_ids   = data.terraform_remote_state.vpc.outputs.public_subnets_ids
-  private_subnets_ids  = data.terraform_remote_state.vpc.outputs.private_subnets_ids
-  instance_type        = var.instance_type
-  bucket_name          = var.bucket_name
-  consul_servers_count = var.consul_servers_count
-  jenkins_nodes_count  = var.jenkins_nodes_count
+  source                = "app.terraform.io/kandula/servers/aws"
+  version               = "1.0.1"
+  vpc_id                = data.terraform_remote_state.vpc.outputs.vpc_id
+  available_zone_names  = data.terraform_remote_state.vpc.outputs.available_zone_names
+  public_subnets_ids    = data.terraform_remote_state.vpc.outputs.public_subnets_ids
+  private_subnets_ids   = data.terraform_remote_state.vpc.outputs.private_subnets_ids
+  instance_profile_name = data.terraform_remote_state.vpc.outputs.instance_profile_name
+  instance_type         = var.instance_type
+  bucket_name           = var.bucket_name
+  consul_servers_count  = var.consul_servers_count
+  jenkins_nodes_count   = var.jenkins_nodes_count
 }
--- Terraform/workspaces/VPC/main.tf
+++ Terraform/workspaces/VPC/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source             = "app.terraform.io/kandula/vpc/aws"
-  version            = "1.0.1"
+  version            = "1.0.2"
   vpc_name           = var.vpc_name
   cidr_size          = var.cidr_size
   availability_zones = var.availability_zones
