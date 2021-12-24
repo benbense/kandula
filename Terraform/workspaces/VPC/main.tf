33965b65251d8b8081dc80e81b1a0f495462387d
unknown <selbiger10@gmail.com>
2021-12-24 23:07:27 +0200

    Updated VPC module version

--- Terraform/workspaces/VPC/main.tf
+++ Terraform/workspaces/VPC/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source             = "app.terraform.io/kandula/vpc/aws"
-  version            = "1.0.4"
+  version            = "1.0.5"
   vpc_name           = var.vpc_name
   cidr_size          = var.cidr_size
   availability_zones = var.availability_zones
