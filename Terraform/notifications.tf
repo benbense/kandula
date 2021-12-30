916663c3975cb9c5c9a0d8a3df268aa70f998b03
unknown <selbiger10@gmail.com>
2021-12-31 00:11:46 +0200

    Added notifications for K8s workspace

--- Terraform/notifications.tf
+++ Terraform/notifications.tf
@@ -28,3 +28,18 @@ resource "tfe_notification_configuration" "vpc_slack_notifications" {
   ]
   url = var.slack_webhook_url
 }
+resource "tfe_notification_configuration" "k8s_slack_notifications" {
+  workspace_id     = module.eks.kubernetes_workspace_id
+  name             = var.kubernetes_workspace_name
+  enabled          = true
+  destination_type = "slack"
+  triggers = [
+    "run:created",
+    "run:planning",
+    "run:needs_attention",
+    "run:applying",
+    "run:completed",
+    "run:errored"
+  ]
+  url = var.slack_webhook_url
+}
