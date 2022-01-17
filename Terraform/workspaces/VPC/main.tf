a3bd3632bc175a2bb3b72162acb5d227dc93ef08
בן זלביגר <selbiger10@gmail.com>
2022-01-17 13:14:37 +0200

    Added missing var reference

--- Terraform/workspaces/VPC/main.tf
+++ Terraform/workspaces/VPC/main.tf
@@ -4,4 +4,6 @@ module "vpc" {
   vpc_name           = var.vpc_name
   cidr_size          = var.cidr_size
   availability_zones = var.availability_zones
+  cert_body = var.cert_body
+  cert_private_key = var.cert_private_key
 }
