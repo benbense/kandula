f0a62f6c131f9341dbd8213639558ef73f242571
unknown <selbiger10@gmail.com>
2022-01-30 17:20:00 +0200

    Removed list

--- Terraform/workspaces/RDS/main.tf
+++ Terraform/workspaces/RDS/main.tf
@@ -8,7 +8,7 @@ resource "aws_db_instance" "postgres" {
   password               = random_password.password.result
   publicly_accessible    = true
   vpc_security_group_ids = [aws_security_group.rds_postgres.id]
-  db_subnet_group_name   = [aws_db_subnet_group.db_subnet_group.name]
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
 }
 
 resource "aws_db_subnet_group" "db_subnet_group" {
