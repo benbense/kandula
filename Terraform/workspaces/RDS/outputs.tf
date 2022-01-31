9eb3ba3b96c5859d55040658e00086c83bcfdc06
unknown <selbiger10@gmail.com>
2022-01-31 15:51:14 +0200

    Added more outputs

--- Terraform/workspaces/RDS/outputs.tf
+++ Terraform/workspaces/RDS/outputs.tf
@@ -2,3 +2,13 @@ output "rds_endpoint" {
   description = "RDS Connection Endpoint"
   value       = module.rds.rds_endpoint
 }
+
+output "rds_address" {
+  description = "RDS Host address"
+  value       = module.rds.rds_address
+}
+
+output "rds_port" {
+  description = "RDS Port"
+  value       = module.rds.rds_port
+}
