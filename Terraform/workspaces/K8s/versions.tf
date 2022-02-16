bd7ce47db813b8b21b4d89a19d3583eb01cf9a92
unknown <selbiger10@gmail.com>
2022-02-16 12:09:51 +0200

    Updated aws provider version from 3.68 to 3.74

--- Terraform/workspaces/K8s/versions.tf
+++ Terraform/workspaces/K8s/versions.tf
@@ -7,12 +7,12 @@ terraform {
       version = "~> 3.1.0"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = ">=2.7.1"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.68.0"
+      version = ">= 3.74.0"
     }
     local = {
       source  = "hashicorp/local"
