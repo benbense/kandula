a5f6d211ad841080cdc305416915f7671acb4f8c
unknown <selbiger10@gmail.com>
2022-03-05 21:39:32 +0200

    Added kube state metrics sg to EKS

--- Terraform/workspaces/K8s/security-groups.tf
+++ Terraform/workspaces/K8s/security-groups.tf
@@ -33,3 +33,21 @@ resource "aws_security_group" "node_exporter_sg_k8s" {
     ]
   }
 }
+
+resource "aws_security_group" "kube_state_metrics_sg_k8s" {
+  name        = "kube_state_metrics_sg_k8s"
+  description = "Security group for Kube State Metrics"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
