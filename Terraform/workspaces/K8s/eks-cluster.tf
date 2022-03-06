015b471c914d5798467dde54ab5a06fcf83c12be
בן זלביגר <selbiger10@gmail.com>
2022-03-06 19:02:22 +0200

    Removed unnessacary Servers remote state

--- Terraform/workspaces/K8s/eks-cluster.tf
+++ Terraform/workspaces/K8s/eks-cluster.tf
@@ -8,16 +8,6 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
-data "terraform_remote_state" "servers" {
-  backend = "remote"
-  config = {
-    organization = "${var.tfe_organization_name}"
-    workspaces = {
-      name = "${var.servers_workspace_name}"
-    }
-  }
-}
-
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
