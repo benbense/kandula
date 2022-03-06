3acebb588b5d9c42ffc0b580d36cdcdf5a92d145
בן זלביגר <selbiger10@gmail.com>
2022-03-06 18:36:29 +0200

    Fixed versions variables

--- Terraform/workspaces/K8s/helm.tf
+++ Terraform/workspaces/K8s/helm.tf
@@ -34,7 +34,12 @@ resource "helm_release" "consul" {
 variable "consul_hosts" {
   default = "provider=aws tag_key=service_role tag_value=service_discovery region=us-east-1"
 }
-variable "consul_chart_version" {
+variable "consul_version" {
   type = string
   default = "1.11.1"
+}
+variable "chart_version" {
+  type        = string
+  description = "The Consul Helm chart version to use"
+  default     = "0.33.0"
 }
